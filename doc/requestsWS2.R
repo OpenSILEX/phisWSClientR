52d4756d15d705552da64a06b8f5bd72d30ca064
Jean-Eudes Hollebecq <jehollebecq@gmail.com>
2020-02-04 10:09:38 +0100

    build vignette POST

--- doc/requestsWS2.R
+++ doc/requestsWS2.R
@@ -1,69 +1,81 @@
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----option, include = FALSE--------------------------------------------------
+knitr::opts_chunk$set( echo = TRUE, message = FALSE, warning = FALSE)
+
+## ----library, echo=TRUE,message=FALSE, warning=FALSE--------------------------
   library(phisWSClientR)
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----connect------------------------------------------------------------------
   # If you want to access to a private web service, you have to insert the address of the WS and the port
-  connectToPHISWS(apiID="ws_private",username="guest@opensilex.org",password="guest", url = "www.opensilex.org/openSilexAPI/rest/")
+  #connectToPHISWS(apiID="ws_private",username="guest@opensilex.org",password="guest", url = "www.opensilex.org/openSilexAPI/rest/")
+  connectToPHISWS(apiID="ws_private",username="guest@opensilex.org",password="guest", url = "http://www.opensilex.org/openSilexSandBoxAPI/rest/")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----token, results = "hide"--------------------------------------------------
   aToken <- opensilexWSClientR::getToken("guest@opensilex.org","guest")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
-  getProjects2( uri="http://www.opensilex.org/demo/PHENOME-FPPN")
+## ----get user info------------------------------------------------------------
+getUserInformations()
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getProject---------------------------------------------------------------
+  getProjects2( acronyme = "PHENOME-FPPN")$data
+
+## ----getExperiments-----------------------------------------------------------
   getExperiments2( uri="http://www.opensilex.org/demo/DIA2017-1")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getScientificObjects-----------------------------------------------------
   getScientificObjects( uri="http://www.opensilex.org/demo/2018/o18000076")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getInfrastructures-------------------------------------------------------
   getInfrastructures(uri="https://emphasis.plant-phenotyping.eu")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getVariables-------------------------------------------------------------
   getVariables2( uri="http://www.opensilex.org/demo/id/variables/v010")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getTraits----------------------------------------------------------------
   getTraits( uri="http://www.opensilex.org/demo/id/traits/t010")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getMethods---------------------------------------------------------------
   getMethods2( uri="http://www.opensilex.org/demo/id/methods/m010")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getUnits-----------------------------------------------------------------
   getUnits( uri="http://www.opensilex.org/demo/id/units/u007")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getProvenances-----------------------------------------------------------
+getProvenances(label = "ImageAnalysis-ZA17-Maize" )
+
+## ----getSpecies---------------------------------------------------------------
   getSpecies( uri="http://www.opensilex.org/id/species/helianthusannuus", language = "en")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getSensors---------------------------------------------------------------
   getSensors( uri="http://www.opensilex.org/demo/2018/s18001")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getVectors---------------------------------------------------------------
   getVectors( serialNumber = "01BD1DD71500001")
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getEvents----------------------------------------------------------------
 # Where to find the "type" URL ?
 # Where is the ontology access  https://github.com/OpenSILEX/ontology-vocabularies/blob/master/oeev.owl
   getEvents( type = "http://www.opensilex.org/vocabulary/oeev#Fertilization", pageSize=10)
 
-## ----echo=TRUE,eval=FALSE,message=FALSE, warning=FALSE-------------------
-#  # Where to find the "type" URL ?
-#  # Where is the ontology access https://github.com/OpenSILEX/ontology-vocabularies/blob/master/oeev.owl
+## ----getAnnotations, eval=FALSE-----------------------------------------------
+#  
 #    getAnnotations( comment = "Ustilago maydis infection" , pageSize=10)
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
-  getEnvironmentData( variable = "http://www.opensilex.org/demo/id/variables/v010", pageSize=50)
+## ----getData------------------------------------------------------------------
+getData(variableUri = "http://www.opensilex.org/demo/id/variables/v014")$data
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----getEnvironmentData-------------------------------------------------------
+  getEnvironmentData( variable = "http://www.opensilex.org/demo/id/variables/v010", pageSize=50)$data
+
+## ----getPhenotypeData---------------------------------------------------------
   head(getPhenotypeData( 
                         variable = "http://www.opensilex.org/demo/id/variables/v001", 
                         pageSize=10)$data)
 
-## ----echo=TRUE,message=FALSE, warning=FALSE------------------------------
+## ----setLog level-------------------------------------------------------------
   setLogLevel("DEBUG")
-  getEnvironmentData( variable = "http://www.opensilex.org/demo/id/variables/v010", pageSize=50)
+  getEnvironmentData( variable = "http://www.opensilex.org/demo/id/variables/v050", pageSize=50)
   setLogLevel("INFO")
 
-## ----session,echo=FALSE,message=FALSE, warning=FALSE---------------------
+## ----session,echo=FALSE-------------------------------------------------------
   sessionInfo()
 
